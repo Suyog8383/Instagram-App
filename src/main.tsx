d162329c99bcec55b1fdc2d30f47a492dd9aba7e
suyog <suyograjenagawade@gmail.com>
2023-01-15 23:07:28 +0530

    did some changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '@fontsource/roboto/300.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
-)
+  </>
+);
